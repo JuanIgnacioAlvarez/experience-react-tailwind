69e51ee03bd1e3af5d21eaa4c5cf9b5232b93b59
Juani <nacho1294.ji@gmail.com>
2023-06-26 17:47:33 -0300

    Subiendo experiencia con React y Tailwind

---
+++ src/components/Plan.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Plan = () => {
+  return (
+    <div className="max-w-[14000px] m-auto py-16 px-4 grid lg:grid-cols-2 gap-4">
+      {/* Left Side */}
+      <div className="grid grid-cols-2 grid-rows-6 h-[80vh]">
+        <img className="row-span-3 object-cover w-full h-full p-2"
+          src="https://images.freeimages.com/images/large-previews/3b2/prague-conference-center-1056491.jpg"
+          alt="/"
+        />
+        <img className="row-span-2 object-cover w-full h-full p-2"
+          src="https://images.freeimages.com/images/large-previews/dee/hotel-in-the-resort-town-of-santa-susana-in-catalonia-near-barcelona-spain-may-2006-1636277.jpg"
+          alt="/"
+        />
+        <img className="row-span-2 object-cover w-full h-full p-2"
+          src="https://images.freeimages.com/images/large-previews/443/german-architecture-1472494.jpg"
+          alt="/"
+        />
+        <img className="row-span-3 object-cover w-full h-full p-2"
+          src="https://images.freeimages.com/images/large-previews/d8b/sea-view-from-hotel-1641395.jpg"
+          alt="/"
+        />
+        <img className="row-span-2 object-cover w-full h-full p-2"
+          src="https://images.freeimages.com/images/large-previews/fe2/maldives-unseen-beauty-1641933.jpg"
+          alt="/"
+        />
+      </div>
+      {/* Right side */}
+      <div className="flex flex-col h-full justify-center">
+        <h3 className="text-5xl md:text-6xl font-bold">Plan Your Next Trip</h3>
+        <p className="text-2xl py-6">
+          Lorem ipsum dolor sit amet concectetur adipisicing elit. Molestias,
+          nam?
+        </p>
+        <p className="pb-6">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod sequi
+          saepe qui blanditiis ea laudantium nostrum culpa alias officiis quia
+          dolorum, repellat sunt accusamus at ipsam praesentium nulla quos
+          harum.
+        </p>
+        <div>
+          <button className="border-black mr-4 hover:shadow-xl">Learn More</button>
+          <button className="bg-black text-white border-black hover:shadow-xl">Book Your Trip</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Plan;
